43aa01a7ae49b01f0d934064b3ae85278fb223d2
Taka Yabe <tyabe@purdue.edu>
2021-05-10 20:34:09 -0400

    commit

--- src/websearch_covidinfodemic_0329/selectdata.java
+++ src/websearch_covidinfodemic_0329/selectdata.java
@@ -10,22 +10,22 @@ import java.util.HashSet;
 
 public class selectdata {
 
-//	public static void main(String[] args) throws IOException {
-		
-		
+	//	public static void main(String[] args) throws IOException {
+
+
 	public static void select_subset(
 			File in, 
 			Integer negsamples
 			) throws NumberFormatException, IOException {	
 
-//		String root = "/mnt/tyabe/"; File root_f = new File(root); root_f.mkdir();
-//		String home = root+"infodemic_0329/"; File home_f = new File(home); home_f.mkdir();
-//		File id_allmeasures     = new File(home+"id_allmeasures.csv");
-//		File id_allmeasures_sub = new File(home+"id_allmeasures_subset.csv");
-//		Integer negsamples = Integer.valueOf(args[0]);
+		//		String root = "/mnt/tyabe/"; File root_f = new File(root); root_f.mkdir();
+		//		String home = root+"infodemic_0329/"; File home_f = new File(home); home_f.mkdir();
+		//		File id_allmeasures     = new File(home+"id_allmeasures.csv");
+		//		File id_allmeasures_sub = new File(home+"id_allmeasures_subset.csv");
+		//		Integer negsamples = Integer.valueOf(args[0]);
 
 		File out = new File(in.getPath().split(".csv")[0]+"_subset.csv");
-		
+
 		BufferedReader br1 = new BufferedReader(new FileReader(in));
 		String line1 = null;
 		Integer ids = 0;
@@ -53,10 +53,15 @@ public class selectdata {
 			String[] tokens = line.split(",");
 			Integer count = Integer.valueOf(tokens[2]);
 			if(!tokens[1].equals("0")) {
+				// if misinfo count > 0 
 				if(count>0) {
-					bw.write(line);
-					bw.newLine();
+					Double rad = Math.random();
+					if(rad<0.3d) {
+						bw.write(line);
+						bw.newLine();
+					}
 				}
+				// if misinfo count == 0
 				else if(count==0) {
 					String mesh = tokens[1].substring(0,8);
 					if(over1mesh.contains(mesh)) {
